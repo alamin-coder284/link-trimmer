02742e8f323ef6ce2dcbf468802bf560f738af81
code-islah <codeislah@gmail.com>
2026-06-19 20:09:21 +0600

    updated link

--- client/src/components/RedirectHandler.jsx
+++ client/src/components/RedirectHandler.jsx
@@ -2,6 +2,6 @@ import { useParams, Navigate } from "react-router-dom";
 
 export default function RedirectHandler() {
   const { code } = useParams();
-  window.location.href = `http://localhost:1234/${code}`;
+  window.location.href = `https://zip9-trimmer.onrender.com/${code}`;
   return <div>Redirecting {code}...</div>;
 }
